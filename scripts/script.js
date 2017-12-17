fc09b473f8f63b6a9136dfa48271adf92cd84a52
TomaszWu <tomasz.wojciechowski84@gmail.com>
2017-12-17 22:21:37 +0100

    main close to finish :)

--- scripts/script.js
+++ scripts/script.js
@@ -18,16 +18,16 @@ $(document).ready(function() {
         );
     })
 
-    $('.product .content a').mouseover(function() {
-        $(this).parent().parent().css({
-            'border': '1px solid black'}
-        );
-    })
-    $('.product .content a').mouseout(function() {
-        $(this).parent().parent().css({
-            'border': '1px solid white'}
-        );
-    })
+    // $('.product .content a').mouseover(function() {
+    //     $(this).parent().parent().css({
+    //         'border': '1px solid black'}
+    //     );
+    // })
+    // $('.product .content a').mouseout(function() {
+    //     $(this).parent().parent().css({
+    //         'border': '1px solid white'}
+    //     );
+    // })
 
 
     $.ajax({
@@ -36,23 +36,70 @@ $(document).ready(function() {
         success: function(result) {
             var footerLi = $('.content-instagram');
             for(var i = 0; i < 6; i++){
-                var pictueToAdd = "<li><img src=" + result.data[i].images.low_resolution.url + "></li>";
+                var pictueToAdd =
+                    "<li>" +
+                    "<div class='instagram-post'>" +
+                        '<a href="' + result.data[i].link + '">' +
+                        "<img src=" + result.data[i].images.low_resolution.url + ">" +
+                        "<div class='instagram-details'>" +
+                            "<div class='instagram-details-handler'>" +
+                            "<i class=\"far fa-heart\"></i><span>" + result.data[i].likes.count + "</span>" +
+                            "<i class=\"far fa-comments\"></i><span>" + result.data[i].comments.count + "</span>" +
+                            "</div>" +
+                        "</div>" +
+                        "</a>"
+                    "</div>" +
+                    "</li>";
                 footerLi.append(pictueToAdd);
             }
+
+            var maxHeight = resizeInstagramHeight();
+            var footerLiImg = $('.content-instagram li div img');
+
+            $.when(footerLiImg.length === 6).then(function(){
+                centerInstaragramPhotos(maxHeight);
+            });
         }
     });
+
+
 
 
     $( window ).resize(function() {
-        var minHeight = 111111110;
-        var footerLi = $('.content-instagram li img');
-        for(var i = 0; i < footerLi.length; i++){
-            if(footerLi[i].clientHeight < minHeight){
-                minHeight = footerLi[i].clientHeight;
-            }
+        var maxHeight = resizeInstagramHeight();
+        centerInstaragramPhotos(maxHeight);
 
-        }
-        $('.content-instagram').css('height', minHeight);
+
     });
+
+    function resizeInstagramHeight(){
+        var maxHeight = Number.MAX_SAFE_INTEGER;
+        var footerLiImg = $('.content-instagram li div img');
+
+        for(var i = 0; i < footerLiImg.length; i++){
+            if(footerLiImg[i].clientHeight < maxHeight){
+                maxHeight = footerLiImg[i].clientHeight;
+            }
+        }
+        $('.content-instagram').css('height', maxHeight);
+
+        return maxHeight;
+    }
+
+    function centerInstaragramPhotos(maxHeight){
+        var footerLiImg = $('.content-instagram li div img');
+
+        for(var i = 0; i < footerLiImg.length; i++){
+            if(footerLiImg[i].clientHeight !== maxHeight){
+                var heightToResize = (footerLiImg[i].clientHeight - maxHeight) / 2;
+                $(footerLiImg[i]).css({
+                    'transform': 'translateY(-' + heightToResize + 'px)',
+                    'top': heightToResize
+                });
+            }
+        }
+    }
+
+
 
 });
